6f64a6332602c36f5ecf95e8e9becd768f6e92de
Konstantin Ivlev <ivlkostya@yandex.ru>
2023-03-28 00:32:54 +1000

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "..\DinamicLib\Leaver.h"
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");   // задаём русский текст
+	system("chcp 1251");            // настраиваем кодировку консоли
+	std::system("cls");
+
+	
+	Leaver lv;
+	std::string userName;
+	std::cout << "Введите имя: "; std::cin >> userName;
+	std::cout << lv.leave(userName) << std::endl;
+
+	system("pause"); // Команда задержки экрана
+
+	return 0;
+}
